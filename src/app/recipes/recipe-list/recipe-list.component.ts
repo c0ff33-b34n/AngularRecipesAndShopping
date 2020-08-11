6b8bdd947e9fb9e3d6366961f158323edab96be3
Stuart Bowes <stuartbowes2000@gmail.com>
2020-08-11 13:12:37 +0100

    Show detail in recipe-detail when selecting recipe in recipe_list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,13 +7,19 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeItemSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2015/11/30/09/34/salad-1069916_960_720.jpg')
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2015/11/30/09/34/salad-1069916_960_720.jpg'),
+    new Recipe('Another Test Recipe', 'This is also simply a test', 'https://cdn.pixabay.com/photo/2020/06/03/18/45/muesli-5255994_960_720.jpg')
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeItemSelected.emit(recipe);
   }
 
 }
