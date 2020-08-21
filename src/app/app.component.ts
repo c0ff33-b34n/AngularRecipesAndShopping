536a70a3f17b857bc143f7608ba90f9b4f5bb015
Stuart Bowes <stuartbowes2000@gmail.com>
2020-08-21 11:16:41 +0100

    Remove onNavigate from app component, as no longer required

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipes-and-shopping';
-  loadedFeature = 'recipe';
-
-  onNavigate(feature: string) {
-    this.loadedFeature = feature;
-  }
 }
