3932e3d564a1698669239e756a96233bf60978c1
Stuart Bowes <stuartbowes2000@gmail.com>
2020-08-28 16:19:40 +0100

    Add auth guard to verify and redirect non logged in users

---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,26 @@
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, UrlTree } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+import { map, take } from 'rxjs/operators';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+
+    constructor(private authService: AuthService, private router: Router) {}
+
+    canActivate(
+        route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot
+        ): boolean | UrlTree | Promise<boolean | UrlTree> | Observable<boolean | UrlTree> {
+        return this.authService.user.pipe(
+            take(1), // to ensure it unsubscribes automatically after 1 use.
+            map(user => {
+            const isAuth = !!user;
+            if (isAuth) {
+                return true;
+            }
+            return this.router.createUrlTree(['/auth']);
+        }));
+    }
+}
