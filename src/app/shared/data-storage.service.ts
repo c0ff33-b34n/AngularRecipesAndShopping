388825a0782defb4b838c722e43f91d92739df07
Stuart Bowes <stuartbowes2000@gmail.com>
2020-08-28 14:24:11 +0100

    Add the user auth token to fetch recipes request

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { map, tap } from 'rxjs/operators';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { map, tap, take, exhaustMap } from 'rxjs/operators';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
     constructor(private http: HttpClient,
-                private recipeService: RecipeService) {}
+                private recipeService: RecipeService,
+                private authService: AuthService) {}
 
     storeRecipes() {
         const recipes = this.recipeService.getRecipes();
@@ -19,16 +21,21 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        return this.http.get<Recipe[]>('https://ng-course-recipes-shopping.firebaseio.com/recipes.json')
-            .pipe(
-                map(recipes => { // rxjs map operator
-                    return recipes.map(recipe => {
-                        return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
-                    }); // JS map array method
-                }),
-                tap(recipes => {
-                this.recipeService.setRecipes(recipes);
-                })
+        return this.authService.user.pipe(take(1), exhaustMap(user => {
+            return this.http.get<Recipe[]>('https://ng-course-recipes-shopping.firebaseio.com/recipes.json',
+            {
+                params: new HttpParams().set('auth', user.token)
+            }
             );
-    }
+        }),
+            map(recipes => { // rxjs map operator
+                return recipes.map(recipe => {
+                    return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+                }); // JS map array method
+            }),
+            tap(recipes => {
+            this.recipeService.setRecipes(recipes);
+            })
+        ); // take will only take 1 value from observable before automatically unsubscribing.
+    } // exhaustMap allows you to combine observables
 }
