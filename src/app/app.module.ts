be425c8bafb2206d5321252594a184e754b84203
Stuart Bowes <stuartbowes2000@gmail.com>
2020-08-31 16:45:06 +0100

    Add lazy loading to recipes and shopping list modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,8 +14,6 @@ import { AuthComponent } from './auth/auth.component';
 import { AuthService } from './auth/auth.service';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { AuthGuard } from './auth/auth.guard';
-import { RecipesModule } from './recipes/recipes.module';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
@@ -30,8 +28,6 @@ import { SharedModule } from './shared/shared.module';
     ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
-    RecipesModule,
-    ShoppingListModule,
     SharedModule
   ],
   providers: [
